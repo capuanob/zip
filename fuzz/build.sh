5cd41431215c5d791611a286b9eac6338f7f1f97
bcapuano <bcapuano@asu.edu>
2024-12-23 07:12:27 -0700

    Testing to fix centipede:none build

--- fuzz/build.sh
+++ fuzz/build.sh
@@ -1,7 +1,13 @@
 cd $SRC/zip
 
 mkdir -p build
-cmake -S . -B build -DCMAKE_C_COMPILER_WORKS=1 -DZIP_BUILD_FUZZ=ON && cmake --build build --target install
+cmake -S . -B build \
+        -DCMAKE_C_COMPILER_WORKS=1 \
+        -DZIP_BUILD_FUZZ=ON \
+        -DCMAKE_C_COMPILER="$CC" \
+        -DCMAKE_C_FLAGS="$CFLAGS" \
+        -DCMAKE_EXE_LINKER_FLAGS="$LIB_FUZZING_ENGINE" \
+    && cmake --build build --target install
 
 # Prepare corpora
 zip -q $OUT/read_entry_fuzzer_seed_corpus.zip fuzz/corpus/*
